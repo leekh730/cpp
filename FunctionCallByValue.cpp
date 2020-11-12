ecddbb20324c7871f5dd6a9fcc362e6cd2a537dc
Kunhee Lee <kunheeuk@naver.com>
2020-11-12 17:40:26 +0900

    functioncallbyvalue_cinNum

--- FunctionCallByValue.cpp
+++ FunctionCallByValue.cpp
@@ -6,7 +6,13 @@ using namespace std;
 extern int sum(int first, int second);
 
 int main(){
-    int first = 100, second = 200, third = 300, result;
+    int first, second, third, result;
+    cout << "Input First Number : ";
+    cin >> first;
+    cout << "Input Second Number : ";
+    cin >> second;
+    cout << "Input Third Number : ";
+    cin >> third;
     result = sum(first, second);
     cout << "Total value is : " << result << endl;
     cout << "Total value is : " << sum(second, third) << endl;
