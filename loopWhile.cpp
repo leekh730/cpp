3d852822cbde4b3ae4b57a3a475debd4c5e3a0a9
Kunhee Lee <kunheeuk@naver.com>
2020-11-12 13:21:48 +0900

    loopwhile_makefunction

--- loopWhile.cpp
+++ loopWhile.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
+int loopwhile();
+
 int main(){
+    int i = loopwhile();
+    return 0;
+}
+
+int loopwhile(){
     int first = 10;
     while(first < 20){
         cout << "Value of first : " << first << endl;
