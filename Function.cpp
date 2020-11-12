184cb1aa7734aa4dc4659dae8b6a22893259e3c7
Kunhee Lee <kunheeuk@naver.com>
2020-11-12 10:47:40 +0900

    function01

---
+++ Function.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int funcInside(); //function declaration, 함수 선언만
+
+int main(){
+    int i = funcInside(); // function call, 어떤 함수를 호출하기 전에는 항상 구성이 된 함수가(선언만 되어 있으면 실행 오류 남!) 먼저 compile되어야 한다. 
+    cout << "return value : " << i << endl;
+    return 0;
+}
+
+int funcInside() { //function definiton, 함수의 구성
+    cout << "int funcInside( )" << endl;
+    return 8; // return 값이 상수이므로, function name 앞에 int가 붙음
+}
+
+/* main{}함수는 항상 존재해야 프로그램 실행이 가능하다.
+main함수 내에 또 다른 함수를 호출하여 사용하는 방법에는 2가지가 있다.
+Import를 해오던가, main함수 전에 호출할 함수를 먼저 작성을 하던가
+Compile은 Top-Down 방식으로 진행된다는 점을 기억하자!
+*/
