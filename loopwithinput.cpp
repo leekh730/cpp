abec7c657294dd3cb49a178a1aa507b66fa7f41d
Kunhee Lee <kunheeuk@naver.com>
2020-11-12 14:28:47 +0900

    input

---
+++ loopwithinput.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int length;
+    cout << "Input length : ";
+    cin >> length;
+    cout << length << endl;
+
+    cout << "Input length : ";
+    cin >> length;
+    cout << length << endl;
+}
