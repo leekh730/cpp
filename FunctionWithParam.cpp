0857c8820c6045c376120cf7e7af5b5d7cac29b1
Kunhee Lee <kunheeuk@naver.com>
2020-11-12 16:39:06 +0900

    functionwithparam_includeMax2

--- FunctionWithParam.cpp
+++ FunctionWithParam.cpp
@@ -7,8 +7,8 @@ extern int max(int, int); // function declaration
 
 int main(){
     int first = 100, second = 200, result;
-    result = max(first, second);
-    cout << "Max value is : " << result << endl;
+    // result = max(first, second);
+    // cout << "Max value is : " << result << endl;
     cout << "Max value is : " << max(second, first) <<endl;
     return 0;
 }
