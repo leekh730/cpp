6f2ada7ffe375b1dfd6f128a812b5991feb5dea8
Kunhee Lee <kunheeuk@naver.com>
2020-11-13 14:27:33 +0900

    makeanduseheaderfile

--- NameSpaces.cpp
+++ NameSpaces.cpp
@@ -15,9 +15,10 @@ namespace second_space{
 
 using namespace first_space;
 
-#include "under/NameSpacesUsingOthers.cpp"
-using namespace third_space;
+// #include "under/NameSpacesUsingOthers.cpp"
+// using namespace third_space;
 
+#include "under/NameSpacesUsingOthers.hpp"
 int main(){
     //func(); //namespace가 first_space이고, 그 안에 들어 있는 func()함수 사용, 이거를 사용할 때는 using namespace first_space를 사용할 것이다라고 명시를 해줘야 함.
     second_space::func(); // namespace가 second_space이고, 그 안에 들어 있는 func()함수 사용
