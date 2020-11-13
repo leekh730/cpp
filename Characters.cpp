e853810da96de9a38b5f1262e417891f4fb57968
Kunhee Lee <kunheeuk@naver.com>
2020-11-13 10:45:08 +0900

    Characters_devidefileandinclude

--- Characters.cpp
+++ Characters.cpp
@@ -1,22 +1,10 @@
 #include <iostream>
+#include "Charactersfunction.cpp"
 using namespace std;
 
 int character();
 
 int main(){
     int x = character();
-    return 0;
-}
-
-int character(){
-    char ch;
-    cin >> ch;
-    if(ch == 'N'){
-        cout << "Press" << ch << "Character!" << endl;
-    }
-    cin >> ch;
-    cout << int(ch) << "Press" << ch << "Character!" << endl;
-    cin >> ch;
-    cout << int(ch) << "Press" << ch << "Character!" << endl;
     return 0;
 }
