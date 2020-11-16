ffb4c9ef926b2be15bf8e7a55ff816b6e5b31cdb
Kunhee Lee <kunheeuk@naver.com>
2020-11-16 13:30:21 +0900

    DataStructures

---
+++ DataStructures.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+struct Book{
+    char title[50], author[50];
+    string subject;
+    int book_id;
+};
+
+int main(){
+    struct Book Book1; //Declare Boo1 of type Book
+    strcpy(Book1.title, "Learn C++ Programming");
+    Book1.subject = "C++ Programming";
+    strcpy(Book1.author, "Chand Miyan");
+    Book1.book_id = 6495407;
+
+    cout << "Book 1 title : " << Book1.title << endl;
+    cout << "Book 1 author : " << Book1.author << endl;
+    cout << "Book 1 subject : " << Book1.subject << endl;
+    cout << "Book 1 id : " << Book1.book_id << endl;
+
+}
