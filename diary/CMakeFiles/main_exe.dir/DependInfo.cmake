1b8bc7094d64ae165146c76c8cb3377e6c32a69f
Kunhee Lee <kunheeuk@naver.com>
2020-11-26 15:44:25 +0900

    CMake

---
+++ diary/CMakeFiles/main_exe.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/rapa/Documents/cpp/diary/calendar.c" "/home/rapa/Documents/cpp/diary/CMakeFiles/main_exe.dir/calendar.c.o"
+  "/home/rapa/Documents/cpp/diary/main.c" "/home/rapa/Documents/cpp/diary/CMakeFiles/main_exe.dir/main.c.o"
+  "/home/rapa/Documents/cpp/diary/memo.c" "/home/rapa/Documents/cpp/diary/CMakeFiles/main_exe.dir/memo.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ diary/CMakeFiles/main_exe.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main_exe.dir/calendar.c.o"
+  "CMakeFiles/main_exe.dir/main.c.o"
+  "CMakeFiles/main_exe.dir/memo.c.o"
+  "main_exe"
+  "main_exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/main_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
